4929dbaf4836d708b1c6fd13de20c5e935382466
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-12-26 18:12:02 -0300

    Componente para jugar de nuevo

---
+++ src/components/Again.jsx
@@ -0,0 +1,24 @@
+// Libraries
+import { useContext } from 'react';
+
+// Context
+import { GameContext } from '../context/GameContext';
+
+// Styles
+import '../styles/again/again.css';
+import '../styles/again/responsive.css';
+
+const Again = () => {
+  const { result, setHasChosen } = useContext(GameContext);
+
+  return (
+    <div className='again'>
+      <p>
+        {result === 'win' ? 'YOU WIN' : result === 'lose' ? 'YOU LOSE' : "IT'S A TIE"}
+      </p>
+      <button style={result === "lose" ? {"color": "red"} : { "color" : "var(--HeaderOutline)"}} onClick={() => setHasChosen(false)}>PLAY AGAIN</button>
+    </div>
+  );
+};
+
+export default Again;
