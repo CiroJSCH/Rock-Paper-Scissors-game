aab1c60288d42ddc07fb37d298d70cbe5e618754
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-12-26 18:13:30 -0300

    Logica para mostrar el play again

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,10 +1,11 @@
 // Libraries
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 // Context
 import { GameContext } from '../context/GameContext';
 
 // Components
+import Again from './Again';
 import ResultButton from './ResultButton';
 
 // Styles
@@ -12,7 +13,10 @@ import '../styles/result/result.css';
 import '../styles/result/responsive.css';
 
 const Result = () => {
-  const { choice, pcChoice, setScore, score } = useContext(GameContext);
+  const { choice, pcChoice, setScore, score, setResult } = useContext(GameContext);
+  const [loading, setLoading] = useState(true);
+
+  const media = window.matchMedia('(min-width: 1200px)');
 
   // Left beats right
   const options = {
@@ -22,23 +26,51 @@ const Result = () => {
   };
 
   useEffect(() => {
-    if (pcChoice.choice === options[choice.choice]) {
-      setScore(score + 1);
-    } else {
-      setScore(score - 1);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+    if (!loading) {
+      if (choice.choice === pcChoice.choice) {
+        setScore(score);
+        setResult("tie");
+      } else if (pcChoice.choice === options[choice.choice]) {
+        setScore(score + 1);
+        setResult("win");
+      } else {
+        setScore(score - 1);
+        setResult("lose");
+      }
     }
-  }, [])
-  
+  }, [loading]);
+
   return (
     <section className='result'>
-      <div className='result__choice'>
-        <ResultButton image={choice.image} choice={choice.choice} />
-        <p>YOU PICKED</p>
+      <div>
+        <div className='result__choice'>
+          <ResultButton image={choice.image} choice={choice.choice} />
+          <p>YOU PICKED</p>
+        </div>
+        {media.matches && (
+        <div>
+          {!loading && <Again />}
+        </div>
+      )}
+        <div className='result__choice'>
+          {loading ? (
+            <div className='selecting'>
+              <span className='selecting__circle'></span>
+            </div>
+          ) : (
+            <ResultButton image={pcChoice.image} choice={pcChoice.choice} />
+          )}
+          <p>THE HOUSE PICKED</p>
+        </div>
       </div>
-      <div className='result__choice'>
-        <ResultButton image={pcChoice.image} choice={pcChoice.choice} />
-        <p>THE HOUSE PICKED</p>
-      </div>
+      {!media.matches && (
+        <div>
+          {!loading && <Again />}
+        </div>
+      )}
     </section>
   );
 };
