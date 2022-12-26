ddc8c0e278e68973ccf20ae0b1547ed5a24ad411
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-12-25 21:48:22 -0300

    Estados

--- src/context/GameContext.jsx
+++ src/context/GameContext.jsx
@@ -1,9 +1,49 @@
-import { createContext } from 'react';
+// Libraries
+import { createContext, useState } from 'react';
+
+// Images
+import rock from '../images/icon-rock.svg';
+import paper from '../images/icon-paper.svg';
+import scissors from '../images/icon-scissors.svg';
 
 export const GameContext = createContext(null);
 
 const GameContextProvider = ({ children }) => {
-  return <GameContext.Provider>{children}</GameContext.Provider>;
+  const images = {
+    rock,
+    paper,
+    scissors,
+  };
+
+  const [choice, setChoice] = useState({});
+  const [hasChosen, setHasChosen] = useState(false);
+  const [pcChoice, setPcChoice] = useState({});
+  const [score, setScore] = useState(0);
+
+  const randomChoice = () => {
+    const random = ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
+    setPcChoice({
+      choice: random,
+      image: images[random],
+    });
+  };
+
+  return (
+    <GameContext.Provider
+      value={{
+        choice,
+        setChoice,
+        hasChosen,
+        setHasChosen,
+        randomChoice,
+        pcChoice,
+        score,
+        setScore
+      }}
+    >
+      {children}
+    </GameContext.Provider>
+  );
 };
 
 export default GameContextProvider;
