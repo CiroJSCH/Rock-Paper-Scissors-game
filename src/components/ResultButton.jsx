4fa8926f133c98848f5296a410ba6140fb6be2f9
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-12-26 19:41:30 -0300

    Verificar si gano para la animacion

--- src/components/ResultButton.jsx
+++ src/components/ResultButton.jsx
@@ -1,10 +1,12 @@
 // Styles
 import "../styles/resultButton/resultButton.css";
 import "../styles/resultButton/responsive.css";
+import '../styles/resultButton/winAnimation.css';
 
-const ResultButton = ({ image, choice }) => {
+const ResultButton = ({ image, choice, winner }) => {
+
   return (
-    <div className='resultButton' id={choice}>
+    <div className={`resultButton ${choice} ${winner ? "win" : null}`}>
       <div className='resultButton--inner'>
         <img src={image} alt='option' />
       </div>
