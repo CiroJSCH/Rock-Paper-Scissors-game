2d0c08947dedc0ad7804ef894d01f133f0cc9575
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-12-26 18:14:17 -0300

    Error de tipeo corregido

--- src/components/ChoiceButton.jsx
+++ src/components/ChoiceButton.jsx
@@ -12,7 +12,7 @@ const ChoiceButton = ({ image, choice }) => {
 
   const { setHasChosen, setChoice, randomChoice } = useContext(GameContext)
 
-  const choseHandler = () => {
+  const choiceHandler = () => {
     setHasChosen(true);
     setChoice({
       image,
@@ -22,7 +22,7 @@ const ChoiceButton = ({ image, choice }) => {
   }
 
   return (
-    <div className="choiceButton" id={choice} onClick={choseHandler}>
+    <div className="choiceButton" id={choice} onClick={choiceHandler}>
       <div className="choiceButton--inner">
         <img src={image} alt="option" />
       </div>
