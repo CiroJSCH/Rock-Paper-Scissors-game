537bbec746b1c6370a6364e56048731f2a84aba1
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-12-27 11:35:47 -0300

    TODO list actualizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+// Libraries
+import { useState } from "react";
+
 // Components
 import Score from "./components/Score";
 import Game from "./components/Game";
+import Modal from "./components/Modal";
+
 // Context
 import GameContextProvider from "./context/GameContext";
 
@@ -9,14 +14,19 @@ import "../src/styles/reset.css";
 import "../src/styles/styles.css";
 
 const App = () => {
+
+  // For rules modal
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="App">
       <GameContextProvider>
         <Score />
         <Game />
-        <button className="rulesButton">
+        <button className="rulesButton" onClick={() => setIsOpen(true)}>
           RULES
         </button>
+        <Modal isOpen={isOpen} setIsOpen={setIsOpen}/>
       </GameContextProvider>
     </div>
   )
@@ -26,5 +36,5 @@ export default App;
 
 //* TODO: PLAY AGAIN COMPONENT
 //* TODO: ANIMACIONES
-// TODO: RULES MODAL
+//* TODO: RULES MODAL
 // TODO: LOCAL STORAGE SCORE
